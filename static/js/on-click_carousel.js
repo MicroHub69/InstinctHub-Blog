5cfa4b63c508a82bdc1a6f681f4b4476664e317b
MicroHub69 <87417063+MicroHub69@users.noreply.github.com>
2022-01-10 12:30:52 +0100

    Cross-Checked

--- static/js/on-click_carousel.js
+++ static/js/on-click_carousel.js
@@ -1,13 +1,37 @@
-const nextBtn = document.querySelector("next-btn");
-const prevBtn = document.querySelector("prev-btn");
-const navigationDots = document.querySelector("navigation-dot");
+// let slidePosition = 0;
+// const slides = document.getElementsByClassName("review_cards");
+// const totalSlides = slides.length;
 
-//set up slider
+// document.getElementById("next-btn").addEventListener("click", function () {
+//   moveToNextSlide();
+// });
 
-function init() {
-  slideTestimony.forEach((reviews, i) => {
-    reviews.style.left = i * 100 + "%";
-  });
-  slideTestimony[0].classList.add("current_card");
-}
-init();
+// document.getElementById("prev-btn").addEventListener("click", function () {
+//   moveToPrevSlide();
+// });
+
+// function updateSlidePosition() {
+//   for (let slide of slides) {
+//     slide.classList.remove(".carousel_item");
+//     slide.classList.add(".carousel-hidden");
+//   }
+//   slides[slidePosition].classList.add(".carousel_item");
+// }
+
+// function moveToNextSlide() {
+//   if (slidePosition === totalSlides - 1) {
+//     slidePosition = 0;
+//   } else {
+//     slidePosition++;
+//   }
+//   updateSlidePosition();
+// }
+
+// function moveToPrevSlide() {
+//   if (slidePosition === 0) {
+//     slidePosition = totalSlides - 1;
+//   } else {
+//     slidePosition--;
+//   }
+//   updateSlidePosition();
+// }
